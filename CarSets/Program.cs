903e59a67b8e934376a360aea6219d7d0e0b19b6
Andy Million <amillion3@gmail.com>
2018-09-10 21:08:25 -0500

    added final methods

--- CarSets/Program.cs
+++ CarSets/Program.cs
@@ -32,25 +32,20 @@ namespace CarSets
                 { "Justy" },
             };
 
+            // This HashSet will contain cars in both the Showroom and Junkyard
             HashSet<string> MatchingCars = new HashSet<string>();
-
             MatchingCars.UnionWith(Showroom);
+
+            // Find cars that are in the Showroom AND the Junkyard
             MatchingCars.IntersectWith(Junkyard);
 
+            // Combine Showroom and Junkyard, no duplicates allowed
+            Showroom.UnionWith(Junkyard);
+
+
+            Junkyard.IntersectWith(MatchingCars);
+
             Console.ReadLine();
-
-            //  Acquiring more cars
-            //Now create another HashSet of cars in a variable Junkyard.Someone who owns 
-            //  a junkyard full of old cars has approached you about buying the entire inventory.
-            //  In the new set, add some different cars, but also add a few that are the 
-            //  same as in the Showroom set.
-
-            //Use the IntersectWith() method to see which cars exist in both the show room 
-            //  and the junkyard.
-            //Now you're ready to buy the cars in the junkyard. Use the UnionWith() method 
-            //  to combine the junkyard into your showroom.
-            //Use the Remove() method to remove any cars that you acquired from the junkyard 
-            //  that you want in your showroom.
         }
     }
 }
